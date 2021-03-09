b9b315feba0b045df0aa6ef31a123d83e49afd3e
William1405 <61747355+William1405@users.noreply.github.com>
2021-03-09 12:00:40 -0500

    Cambio logo carrito con funcionalidades

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var splash, home_menu_restaurante, bebidas, sandwichs, hamburguesas, ver_pedido, iniciarSesion, crearMiCuenta, ingresarComoInvitado, desplegable; /*Estas son las secciones face,*/
-			var btn_bebidas, btn_sandwichs, btn_hamburguesas, btn_ver_pedido, btn_face, btns_volver, btns_iniciarSesion, btns_crearMiCuenta, btn_ingresarComoInvitado, btn_desplegable; /*btns_volver es un ARRAY de botones*/
+var splash, home_menu_restaurante, bebidas, sandwichs, hamburguesas, ver_pedido, iniciarSesion, crearMiCuenta, ingresarComoInvitado, login, desplegable; /*Estas son las secciones face,*/
+			var btn_bebidas, btn_sandwichs, btn_hamburguesas, btn_ver_pedido, btn_appi, btns_volver, btns_iniciarSesion, btns_crearMiCuenta, btn_ingresarComoInvitado, btn_desplegable; /*btns_volver es un ARRAY de botones*/
 			var secciones;
 			var flipCheck = 0;
 
@@ -19,22 +19,23 @@ var splash, home_menu_restaurante, bebidas, sandwichs, hamburguesas, ver_pedido,
 				sandwichs = document.getElementById("sandwichs");
 				hamburguesas = document.getElementById("hamburguesas");
 				ver_pedido = document.getElementById("ver_pedido");
+				login = document.getElementById("login");
 				/*face = document.getElementById("face");*/
 				desplegable  = document.getElementById("desplegable");
 				iniciarSesion = document.getElementById("iniciarSesion");
 				crearMiCuenta = document.getElementById("crearMiCuenta");
 				ingresarComoInvitado = document.getElementById("ingresarComoInvitado");
-				secciones = [splash, home_menu_restaurante, bebidas, sandwichs, hamburguesas, ver_pedido, face, iniciarSesion, crearMiCuenta, ingresarComoInvitado, desplegable];
+				secciones = [splash, home_menu_restaurante, bebidas, sandwichs, hamburguesas, ver_pedido, login, iniciarSesion, crearMiCuenta, ingresarComoInvitado, desplegable];
 				btn_bebidas = document.getElementById("btn_bebidas");
 				btn_sandwichs = document.getElementById("btn_sandwichs");
 				btn_hamburguesas = document.getElementById("btn_hamburguesas");
-				btn_ver_pedido = document.getElementById("btn_ver_pedido");
-				/*btn_face = document.getElementById("btn_face");*/
+				//btn_ver_pedido = document.getElementById("btn_ver_pedido");
+				/*btn_app = document.getElementById("btn_app");*/
 				btn_iniciarSesion = document.getElementById("btn_iniciarSesion");
 				btn_crearMiCuenta = document.getElementById("btn_crearMiCuenta");
 				btn_ingresarComoInvitado = document.getElementById("btn_ingresarComoInvitado");
 				btns_volver = document.querySelectorAll(".volver");
-				btn_face = document.querySelectorAll(".face");
+				btn_appi = document.querySelectorAll(".inicio");
 				btn_desplegable = document.getElementById("btn_desplegable");
 			}
 
@@ -42,24 +43,27 @@ var splash, home_menu_restaurante, bebidas, sandwichs, hamburguesas, ver_pedido,
 				btn_bebidas.addEventListener("click",()=>{irA(bebidas);});
 				btn_sandwichs.addEventListener("click",()=>{irA(sandwichs);});
 				btn_hamburguesas.addEventListener("click",()=>{irA(hamburguesas);});
-				btn_ver_pedido.addEventListener("click",()=>{irA(ver_pedido);});
-				/*btn_face.addEventListener("click",()=>{irA(face);});*/
+				//btn_ver_pedido.addEventListener("click",()=>{irA(ver_pedido);});
+				/*btn_app.addEventListener("click",()=>{irA(face);});*/
 				btn_iniciarSesion.addEventListener("click",()=>{irA(iniciarSesion);});
 				btn_crearMiCuenta.addEventListener("click",()=>{irA(crearMiCuenta);});
 				btn_ingresarComoInvitado.addEventListener("click",()=>{irA(ingresarComoInvitado);});
 				btn_desplegable.addEventListener("click",()=>{irA(desplegable);});
 				//btn_volver.addEventListener("click",()=>{irA(home_menu_restaurante);});
 
-				for (var i=0;i<=7;i++) {
+				for (var i=0;i<=2;i++) 
+				{
 					//console.log(btns_volver,i);
+					
 					btns_volver[i].addEventListener("click",()=>{irA(home_menu_restaurante);});
-					btn_face[i].addEventListener("click",()=>{irA(home_menu_restaurante);});
+					
 				}
-				/*
-				for (var i=0;i<=7;i++) {
+				
+				for (var i=0;i<=6;i++) 
+				{
 					//console.log(btns_volver,i);
-					btn_face[i].addEventListener("click",()=>{irA(home_menu_restaurante);});
-				}*/
+					btn_appi[i].addEventListener("click",()=>{irA(ver_pedido);});
+				}
 				
 				
 				//btns_volver.addEventListener("click",()=>{irA(home_menu_restaurante);});
